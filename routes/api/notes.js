ae56c42f6fb52484efbe3bab1c43156c1cb83598
Rona C <rona.chen@hotmail.com>
2023-06-01 19:41:45 -0400

    fixed src for script and css in .html files

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 const { readAndAppend, readFromFile } = require('../../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 router.post('/', (req, res) => {
-  console.log("Adding the notes!📖")
-  console.log(req.body)
   const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
       title,
       text,
+      id: uuidv4(),
     };
     readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully! 📖`);
--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 
+// const homeRoutes = require('./routes/homeRoutes')
+// const apiRoutes = require("./routes/api/notes")
+
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+// app.use("/", homeRoutes);
+// app.use("/api", apiRoutes);
 app.use(routes);
 
 app.listen(PORT, () =>
