f74679e3a96b02d27fef3522fd5e5524e5d78f09
Rona C <rona.chen@hotmail.com>
2023-06-01 21:33:08 -0400

    added the delete http request methods

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,11 +1,12 @@
 // Import required dependencies
 const router = require('express').Router();
-const { readAndAppend, readFromFile } = require('../../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile} = require('../../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 // Route for reading the content and send the response in JSON formatted data
 router.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
 });
 
 // Route for recieveing a POST request
@@ -19,11 +20,23 @@ router.post('/', (req, res) => {
       id: uuidv4(),
     };
     readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully! 📖`);
+    res.json('Note added successfully! 📖');
   } else {
     res.error('Oh no, something went wrong!');
   }
 });
+
+// Route for recieving a DELETE request for the selected note from the array
+router.delete('/:id', (req, res) => {
+  const noteId  = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const newNotes = json.filter((note) => note.id !== noteId);
+      writeToFile('./db/db.json', newNotes);
+      res.json("Note has been deleted 🗑️");
+    });
+})
 
 // Export the router module 
 module.exports = router;
