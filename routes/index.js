1d2680a18731003e0771823c13ccc00e0f283176
Rona C <rona.chen@hotmail.com>
2023-06-01 20:39:56 -0400

    updated comments in teh server.js and index.js for routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+// Import router module from Express
 const router = require("express").Router()
 
+// Import route modules
 const homeRoutes = require('./homeRoutes')
 const apiRoutes = require("./api")
 
+// Mounting the apiRoutes module on the "/api" path and homeRoutes module on the root path ("/")
 router.use("/api", apiRoutes)
 router.use("/", homeRoutes)
 
+// Export the router module 
 module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-// Import routes and create an express application
+// Import the Express module and route modules
 const express = require('express');
-const app = express();
 const routes = require('./routes');
+
+// Create an express application
+const app = express();
 
 // Set the user environment
 const PORT = process.env.PORT || 3001;
 
-// Middleware for parsing JSON and urlencoded form data, and serve static files
+// Middleware for parsing JSON
 app.use(express.json());
+
+// Middleware for URL-encoded form data
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware for serving static files from the 'public' directory
 app.use(express.static('public'));
+
+// Mounting the routes module to handle incoming requests
 app.use(routes);
 
 // Listen for connections
